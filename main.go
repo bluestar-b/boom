ae7bd36f73825f70a798a29252a9e16bc8b0d371
Mac <macdev2001@gmail.com>
2023-10-22 20:18:37 +0700

    unknown release 1.1.0

--- main.go
+++ main.go
@@ -4,42 +4,50 @@ import (
     "flag"
     "fmt"
     "sync"
+    "time"
     "github.com/valyala/fasthttp"
-
 )
 
-
-func CreateRequest(url string, method string) ([]byte, int) {
+func CreateRequest(url string, method string) (int, time.Duration) {
     req := fasthttp.AcquireRequest()
     defer fasthttp.ReleaseRequest(req)
 
     req.Header.SetMethod(method)
     req.SetRequestURI(url)
 
+    start := time.Now()
     resp := fasthttp.AcquireResponse()
     defer fasthttp.ReleaseResponse(resp)
 
     if err := fasthttp.Do(req, resp); err != nil {
-        return []byte{}, 0
+        return 0, time.Since(start)
     }
 
-    return resp.Body(), resp.StatusCode()
+    return resp.StatusCode(), time.Since(start)
 }
 
+func Flood(reqcount int, url string, mode string) (int, int) {
+    successCount := 0
+    failureCount := 0
 
-func Flood(reqcount int, url string, mode string) {
     for i := 0; i < reqcount; i++ {
-        _, getStatusCode := CreateRequest(url, mode)
-        fmt.Printf("status: %d, %s\n", getStatusCode, url)
+        getStatusCode, requestTime := CreateRequest(url, mode)
+        if getStatusCode == 200 {
+            successCount++
+        } else {
+            failureCount++
+        }
+        fmt.Printf("Request %d - Status: %d, Time: %s\n", i+1, getStatusCode, requestTime)
     }
 
+    return successCount, failureCount
 }
 
 func main() {
-    countPtr := flag.Int("count", 8, "Number of goroutines")
-    reqCountPtr := flag.Int("reqcount", 3, "Number of requests per goroutine")
-    urlPtr := flag.String("url", "", "URL to request")
-    modePtr := flag.String("mode", "", "HTTP request method")
+    countPtr := flag.Int("c", 8, "Number of goroutines")
+    reqCountPtr := flag.Int("rc", 1, "Number of requests per goroutine")
+    urlPtr := flag.String("h", "", "URL to request")
+    modePtr := flag.String("m", "", "HTTP request method")
     flag.Parse()
 
     if *urlPtr == "" || *modePtr == "" {
@@ -48,16 +56,24 @@ func main() {
     }
 
     var wg sync.WaitGroup
+    totalSuccess := 0
+    totalFailure := 0
 
     for i := 0; i < *countPtr; i++ {
         go func() {
             defer wg.Done()
-            Flood(*reqCountPtr, *urlPtr, *modePtr)
+            success, failure := Flood(*reqCountPtr, *urlPtr, *modePtr)
+            totalSuccess += success
+            totalFailure += failure
         }()
         wg.Add(1)
     }
 
     wg.Wait()
-    fmt.Println("\nDone")
+
+    totalRequests := *countPtr * *reqCountPtr
+    fmt.Printf("\nTotal Requests: %d\n", totalRequests)
+    fmt.Printf("Successful Requests: %d (%.2f%%)\n", totalSuccess, float64(totalSuccess)/float64(totalRequests)*100)
+    fmt.Printf("Failed Requests: %d (%.2f%%)\n", totalFailure, float64(totalFailure)/float64(totalRequests)*100)
 }
 
